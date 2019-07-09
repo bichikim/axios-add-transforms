b372f39c321cffcb53de05ff7f2b0521e16f8f28
BichiKim <bichi@live.co.kr>
2019-07-09 16:12:31 +0900

    update version

---
+++ declarations/index.d.ts
@@ -0,0 +1,26 @@
+import { AxiosRequestConfig, AxiosTransformer, Method } from 'axios';
+export interface TransformSetArray {
+    request: AxiosTransformer[];
+    response: AxiosTransformer[];
+}
+export interface TransformSet {
+    request?: AxiosTransformer | AxiosTransformer[];
+    response?: AxiosTransformer | AxiosTransformer[];
+}
+export interface Matcher {
+    test: RegExp;
+    method?: 'all' | 'ALL' | Method;
+    transform: TransformSet;
+}
+export interface TransformsOptions {
+    first?: TransformSet;
+    final?: TransformSet;
+    matchers?: Matcher[];
+}
+export default class Transforms {
+    static confirmTransforms(transformSet?: TransformSet): TransformSetArray;
+    private readonly _options;
+    constructor(options: TransformsOptions);
+    readonly matchers: Matcher[];
+    addTransforms(config: AxiosRequestConfig): AxiosRequestConfig;
+}
